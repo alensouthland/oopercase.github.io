e411e6e111353cb16d48358585c79cd71a5db6ad
alensouthland <alensouthland@gmail.com>
2026-04-02 12:20:54 +0530

    tweaks and contact email

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,9 @@
 function About() {
+
+  const handleEmailClick = () => {
+    window.location.href = "mailto:alensouthland@gmail.com";
+  };
+
   return (
     <div className="page_container">
       <h1 style={{ fontFamily: 'Edmund, serif', fontSize: 'clamp(2rem, -1.5rem + 8vw, 3rem)' }}>Hello, I'm <span> <a style={{ color: 'var(--text-color)', borderBottom: '2px solid var(--text-color)' }} href="https://alen.framer.website">Alen</a></span></h1>
@@ -13,6 +18,9 @@ function About() {
         <p style={{ maxWidth: '48ch', marginBottom: '1rem', fontFamily: 'VerumSans, sans-serif', fontSize: '1rem', textWrap: 'pretty' }}>
           OOPER CASE TYPE is a library of fonts created by me. This project is actively in development adding new fonts and features. Built with ❤️ using React with Claude.
         </p>
+        <button onClick={handleEmailClick}>
+          alensouthland@gmail.com
+        </button>
       </footer>
 
     </div>
